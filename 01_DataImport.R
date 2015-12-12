f670e3b7359a9679efabb997802045c3f66fb60a
faying <ffay10@gmail.com>
2015-12-13 01:21:53 +0800

    Impute Data

--- 01_DataImport.R
+++ 01_DataImport.R
@@ -1,10 +1,31 @@
 ####Import Data
-dat <- read.csv("Data/hypothyroid_1109.csv")
+dat_orginal <- dat <- read.csv("Data/hypothyroid_1212.csv")
+
+# dat$hypothyroid <- factor(dat$hypothyroid, levels=c(0,1), labels=c("FALSE","TRUE"))
+dat$hypothyroid <- factor(dat$hypothyroid)
+dat$sex <- factor(dat$sex)
+dat$on_thyroxine <- factor(dat$on_thyroxine)
+dat$query_on_thyroxine <- factor(dat$query_on_thyroxine)
+dat$on_antithyroid_medication <- factor(dat$on_antithyroid_medication)
+dat$thyroid_surgery <- factor(dat$thyroid_surgery)
+dat$query_TRUE <- factor(dat$query_TRUE)
+dat$query_hyperthyroid <- factor(dat$query_hyperthyroid)
+dat$pregnant <- factor(dat$pregnant)
+dat$sick <- factor(dat$sick)
+dat$tumor <- factor(dat$tumor)
+dat$lithium <- factor(dat$lithium)
+dat$goitre <- factor(dat$goitre)
+dat$TSH_measured <- factor(dat$TSH_measured)
+dat$T3_measured <- factor(dat$T3_measured)
+dat$TT4_measured <- factor(dat$TT4_measured)
+dat$T4U_measured <- factor(dat$T4U_measured)
+dat$FTI_measured <- factor(dat$FTI_measured)
+dat$TBG_measured <- factor(dat$TBG_measured)
 
 ####Data Summary
 library(Hmisc)
+summary(dat)
 describe(dat)
-sum(complete.cases(dat))
 
 ####Finding Missing Data
 library(mice)
@@ -29,7 +50,8 @@ histMiss(dat[,c("age","TSH")])
 
 # names(dat)
 library(VIM)
-aggr_plot <- aggr(dat, col=c('navyblue','red'), numbers=TRUE, sortVars=TRUE, labels=names(data), cex.axis=.7, gap=3, ylab=c("Histogram of missing data","Pattern"))
+aggr_plot <- aggr(dat, col=c('navyblue','red'), numbers=TRUE, sortVars=TRUE, labels=names(data), 
+                  cex.axis=.7, gap=3, ylab=c("Histogram of missing data","Pattern"))
 marginplot(dat[,c("hypothyroid","TBG")])
 marginplot(dat[,c("hypothyroid","FTI")])
 marginplot(dat[,c("hypothyroid","T4U")])
@@ -40,3 +62,26 @@ marginplot(dat[,c("hypothyroid","age")])
 matrixplot(dat)  
 marginmatrix(dat[,c("hypothyroid","TBG","FTI","T4U","TT4","T3","TSH","age")])
 pbox(dat, pos=2)
+
+
+####Data Cleaning
+library(dplyr)
+# 刪除沒有一個有效化驗值的樣本（3163->3162）
+tmp <- filter(dat,!((dat$TSH<=0 | is.na(dat$TSH)) & (dat$T3<=0 | is.na(dat$T3)) & (dat$TT4<=0 | is.na(dat$TT4)) & 
+                      (dat$T4U<=0 | is.na(dat$T4U)) & (dat$FTI<=0 | is.na(dat$FTI)) & (dat$TBG<=0 | is.na(dat$TBG))))
+
+# 刪除測量T3但是值不大於0或為空的樣本(3162->3161)
+tmp <- filter(tmp,!(tmp$T3_measured==1 & (tmp$T3<=0 | is.na(tmp$T3))))
+# 刪除測量T4U但是值不大於0或為空的樣本(3161->3159)
+tmp <- filter(tmp,!(tmp$T4U_measured==1 & (tmp$T4U<=0 | is.na(tmp$T4U))))
+# 刪除測量FTI但是值不大於0或為空的樣本(3159->3151)
+tmp <- filter(tmp,!(tmp$FTI_measured==1 & (tmp$FTI<=0 | is.na(tmp$FTI))))
+# 刪除測量TSH但是值不大於0或為空的樣本(3151->2258)
+tmp <- filter(tmp,!(tmp$TSH_measured==1 & (tmp$TSH<=0 | is.na(tmp$TSH))))
+
+table(dat$hypothyroid)
+table(tmp$hypothyroid)
+# 刪除TBG測量值
+dat <- select(tmp,-TBG)
+
+rm(dat_1)
--- 02_Imputation.R
+++ 02_Imputation.R
@@ -1,12 +1,11 @@
-####
-dat_old <- dat
-dat <- dat[,-26]
 ####Iterative robust model-based imputation (IRMI)
+library(VIM)
 dat_irmi <- irmi(dat)
 
 ####bootstrap EM algorithm
 library(Amelia)
-dat_amelia <- amelia(x=dat[,c("hypothyroid","age","sex","TT4")],cs=c("hypothyroid","sex")) 
+AmeliaView()
+dat_amelia <- amelia(x=dat) 
 
 # ####
 # library(mvnmle)
@@ -17,12 +16,12 @@ dat_amelia <- amelia(x=dat[,c("hypothyroid","age","sex","TT4")],cs=c("hypothyroi
 # dat_rrcov <- impSeq(dat)
 
 ####Multivariate Imputation by Chained Equations
+library(mice)
 mid_mice <- mice(dat)
 mid_mice$imp$age
-mid_mice_2 <- complete(mid_mice,'long',inc=TRUE)
-col <- rep(c("blue", "red")[1+as.numeric(is.na(mid_mice$data$age))],6)
-stripplot(age~.imp, data=mid_mice_2, jit=TRUE,col=col, xlab="imputation Number")
+dat_mice <- complete(mid_mice)
 
+####MCMC
 library(mi)
 mdf <- missing_data.frame(dat)
 show(mdf)
@@ -30,4 +29,32 @@ summary(mdf)
 image(mdf)
 hist(mdf)
 options(mc.cores=8)
-imp <- mi(mdf,n.iter = 30, n.chains = 4, max.minutes = 20)
+imp <- mi(mdf,n.chains=8)
+show(imp)
+round(mipply(imp, mean, to.matrix = TRUE), 3)
+Rhats(imp)
+imp <- mi(imp, n.iter = 5)
+plot(imp)
+image(imp)
+summary(imp)
+names(dat)
+analysis <- pool(hypothyroid ~ age + sex + on_thyroxine + query_on_thyroxine + on_antithyroid_medication + thyroid_surgery + 
+                   query_TRUE + query_hyperthyroid + pregnant + sick + tumor + lithium + goitre + TSH_measured + 
+                   TSH + T3_measured + T3 + TT4_measured + TT4 + T4U_measured + T4U + FTI_measured + FTI +
+                   TBG_measured, data = imp, m = 5)
+display(analysis)
+mid_imp <- complete(imp,1)
+dat_imp <- select(mid_imp,-missing_age,-missing_sex,-missing_TSH,-missing_T3,-missing_TT4,-missing_T4U,-missing_FTI)
+
+
+####
+ls()
+rm(mdf)
+rm(mid_imp)
+rm(tmp)
+rm(aggr_plot)
+rm(analysis)
+rm(dat_amelia)
+rm(i)
+rm(imp)
+rm(mid_mice)
